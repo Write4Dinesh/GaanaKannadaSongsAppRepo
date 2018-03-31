9dc52946520b9d1ed525d252fd6af4e3b2e581e5
DineshAtMac <write4dinesh@gmail.com>
2018-04-01 03:52:05 +0530

    renamed the project folder

---
+++ KannadaOldSongs/app/src/main/java/com/shrinvi/ssr/ui/KNABrowserActivity.java
@@ -0,0 +1,98 @@
+package com.shrinvi.ssr.ui;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.shrinvi.ssr.analytics.KNAGoogleAnalytics;
+import com.shrinvi.ssr.model.KNAConstants;
+import com.shrinvi.ssr.model.KNAUtils;
+import com.shrinvi.ssr.storage.KNADataStore;
+import com.shrinvi.ssr.model.KNAWebViewClient;
+import com.shrinvi.ssr.R;
+
+public class KNABrowserActivity extends KNASuperActivity {
+    private WebView mKNAWebView;
+    private InterstitialAd interstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_browser);
+        KNAUtils.configLocale(this);
+        mKNAWebView = findViewById(R.id.kna_webview);
+        mKNAWebView.getSettings().setAllowContentAccess(true);
+        mKNAWebView.getSettings().setJavaScriptEnabled(true);
+        mKNAWebView.getSettings().setAppCacheEnabled(true);
+        mKNAWebView.getSettings().setSupportZoom(true);
+        mKNAWebView.getSettings().setBuiltInZoomControls(true);
+        mKNAWebView.getSettings().setDisplayZoomControls(false);
+        mKNAWebView.getSettings().setDomStorageEnabled(true);
+        mKNAWebView.getSettings().setSupportMultipleWindows(true);
+
+        mKNAWebView.setWebViewClient(new KNAWebViewClient((ProgressBar) findViewById(R.id.progress_spinner)));
+
+        String url = getIntent().getStringExtra(KNAConstants.INTENT_EXTRA_URL);
+        String title = getIntent().getStringExtra(KNAConstants.INTENT_EXTRA_TITLE);
+        if (title != null && !title.isEmpty()) {
+            getSupportActionBar().setTitle(title);
+        }
+        if (url != null && !url.isEmpty()) {
+            mKNAWebView.loadUrl(url);
+        }
+        initBannerAd();
+        initInterstitialAd();
+    }
+
+    private void initInterstitialAd() {
+        // Create the InterstitialAd and set the adUnitId.
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId(getString(R.string.ad_unit_id_for_browse_interstitial));
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdClosed() {
+                finish();//close browser activity
+            }
+        });
+        AdRequest adRequest = new AdRequest.Builder().build();
+        interstitialAd.loadAd(adRequest);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mKNAWebView.canGoBack()) {
+            mKNAWebView.goBack();
+        } else {
+            showInterstitialAd();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        KNAGoogleAnalytics.sendScreenViewEvent("Browser Screen");
+    }
+
+    private void initBannerAd() {
+        AdView adView = findViewById(R.id.browser_adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+    }
+
+    private void showInterstitialAd() {
+        long timeElapsedInMillis = System.currentTimeMillis() - KNADataStore.getInstance(this).getLastAddShowedTime();
+        if (interstitialAd != null && interstitialAd.isLoaded() && (timeElapsedInMillis > 60 * 1000)) {
+            KNAGoogleAnalytics.sendCustomEvent("AdMob", "ShowInterstitial");
+            interstitialAd.show();
+            KNADataStore.getInstance(this).storeLastAddShowedTime(System.currentTimeMillis());
+        } else {
+            KNAGoogleAnalytics.sendCustomEvent("AdMob", "ShowInterstitial:not loaded yet");
+            finish();
+
+        }
+    }
+}
---
+++ KannadaOldSongs/app/src/main/java/com/shrinvi/ssr/ui/KNASplashActivity.java
@@ -0,0 +1,46 @@
+package com.shrinvi.ssr.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+import com.google.android.gms.ads.MobileAds;
+import com.shrinvi.ssr.analytics.KNAGoogleAnalytics;
+import com.shrinvi.ssr.model.KNAUtils;
+import com.shrinvi.ssr.R;
+
+public class KNASplashActivity extends KNASuperActivity {
+    public static final long LOAD_ANIMATION_DELAY_IN_MILLIS = 2 * 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        KNAUtils.configLocale(this);
+        setContentView(R.layout.activity_splash);
+        ImageView loadingIv = findViewById(R.id.splash_image_view);
+        loadingIv.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                launchHomeScreen();
+            }
+        }, LOAD_ANIMATION_DELAY_IN_MILLIS);
+        MobileAds.initialize(this, getString(R.string.admob_app_id));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        KNAGoogleAnalytics.sendScreenViewEvent("Splash Screen");
+    }
+
+    private void launchHomeScreen() {
+        Intent homeIntent = new Intent(this, KNAHomeActivity.class);
+        startActivity(homeIntent);
+        finish();
+    }
+}
